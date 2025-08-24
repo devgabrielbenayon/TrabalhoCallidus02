34f3da8bf113b884f2ab2c0ee11da8ad162c8c88
ArthurMCR <amcr.snf24@uea.edu.br>
2025-08-24 18:26:11 -0400

    feat(auth): adicionado user model, manager e auth routes básicas para autenticação

---
+++ backend/app/api/auth_routes.py
@@ -0,0 +1,57 @@
+from flask import Blueprint, jsonify, request
+from app.models.User import User
+from app.models.UserManager import UserManager
+from datetime import datetime
+import jwt
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+manager = UserManager("backend/testes/users.json")
+
+SECRET_KEY = "chave_segredo"
+
+
+#POST registro de usuário
+@bp.route('/register', methods=['POST'])
+def register():
+    data = request.json
+
+    if not data.get("username") or not data.get("password"):
+        return jsonify({"error": "É obrigatório ter nome de usuário e senha"}), 400
+    
+    if manager.find_by_username(data["username"]):
+        return jsonify({"error": "Usuário já existe, tente outro nome"}), 400
+
+    new_user = User(username=data["username"])
+    new_user.set_password(data["password"])
+
+    users = manager.load_users()
+    users.append(new_user)
+    manager.save_users(users)
+
+    return jsonify(new_user.to_dict()), 201
+
+
+#POST login de usuário
+@bp.route("/login", methods=["POSTS"])
+def login():
+    data = request.json
+    if not data.get("username") or not data.get("password"):
+        return jsonify({"error": "É obrigatório ter nome de usuário e senha"}), 400
+    
+    user = manager.find_by_username(data["username"])
+    if not user or not user.check_password(data["password"]):
+        return jsonify({"error": "Credenciais inválidas"}), 401
+    
+    token = jwt.encode(
+        {
+            "user_id": user.id,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+        },
+        SECRET_KEY,
+        algorithm="HS256"
+    )
+
+    return jsonify({"token": token})
+    
+    
